060f99380f83493ecc3e234d8db7c84d3be2ce2f
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-31 11:18:21 -0800

    Lesson 9 - Todo App (Part 1)

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from './components/Header';
 
 const App = () => {
+	const [todos, setTodos] = useState([
+		{ text: 'buy coffee', key: '1' },
+		{ text: 'create an app', key: '2' },
+		{ text: 'play bideo bames', key: '3' },
+	]);
+
 	return (
 		<View style={styles.container}>
+			<Header />
+			<View style={styles.content}>
+				{/* todo form */}
+				<View style={styles.list}>
+					<FlatList
+						data={todos}
+						renderItem={({ item }) => <Text>{item.text}</Text>}
+					/>
+				</View>
+			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -14,6 +32,12 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
+	},
+	content: {
+		padding: 40,
+	},
+	list: {
+		marginTop: 20,
 	},
 });
 
