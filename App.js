9eba38442db028becfa9c9ded485e6da3aae6452
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-31 10:56:36 -0800

    Lesson 7 - Flat List Component

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 const App = () => {
 	const [people, setPeople] = useState([
-		{ name: 'shaun', key: '1' },
-		{ name: 'yoshi', key: '2' },
-		{ name: 'mario', key: '3' },
-		{ name: 'luigi', key: '4' },
-		{ name: 'peach', key: '5' },
-		{ name: 'toad', key: '6' },
-		{ name: 'bowser', key: '7' },
+		{ name: 'shaun', id: '1' },
+		{ name: 'yoshi', id: '2' },
+		{ name: 'mario', id: '3' },
+		{ name: 'luigi', id: '4' },
+		{ name: 'peach', id: '5' },
+		{ name: 'toad', id: '6' },
+		{ name: 'bowser', id: '7' },
 	]);
-
+	/*
+    FlatList automatically looks for and adds 'key' on any JSX we are rendering iteratively
+    FlatList will only render a certain amount of items whereas ScrollView will render everything; FlatList has better performance?
+    If the data given to FlatList doesn't have a key property on each item, we can use the keyExtractor attribute to tell FlatList what property to use as the key from each item
+  */
 	return (
 		<View style={styles.container}>
-			<ScrollView>
+			<FlatList
+				numColumns={2}
+				keyExtractor={(item) => item.id}
+				data={people}
+				renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+			/>
+			{/* <ScrollView>
 				{people.map((person) => (
 					<View key={person.key}>
 						<Text style={styles.item}>{person.name}</Text>
 					</View>
 				))}
-			</ScrollView>
+			</ScrollView> */}
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -41,6 +51,8 @@ const styles = StyleSheet.create({
 		padding: 30,
 		backgroundColor: 'pink',
 		fontSize: 24,
+		marginHorizontal: 10,
+		marginTop: 24,
 	},
 });
 
