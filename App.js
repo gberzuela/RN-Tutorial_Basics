0e05047f0f7d0679d1755b97433ce0888dfea78a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-31 10:17:21 -0800

    lesson 3 - views, text, & styles

--- App.js
+++ App.js
@@ -2,20 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.boldText}>Hello world!</Text>
+			</View>
+			<View style={styles.body}>
+				<Text>Testing for React Native Tutorial #3</Text>
+				<Text>React Native is fun! (I think)</Text>
+				<Text>Let's get brazy</Text>
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	body: {
+		backgroundColor: 'yellow',
+		padding: 20,
+	},
+	boldText: {
+		fontWeight: 'bold',
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	header: {
+		backgroundColor: 'pink',
+		padding: 20,
+	},
 });
+
+export default App;
